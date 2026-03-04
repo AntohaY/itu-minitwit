64b5ab25c7d1cc2041de6ed495af43601f263296
Mengdi Liao <106358515+mengdiliao@users.noreply.github.com>
2026-03-04 13:16:34 +0100

    Revert "Enhance/release automation"

--- new/main.go
+++ new/main.go
@@ -2,7 +2,7 @@
 
 //1. you can do docker composition
 //docker-compose up --build
-//docker composition is set for port 8080! -> http://localhost:8080
+//docker composition is set for port 5001! -> http://localhost:5001
 
 //2. do it manually but than don't do step 1
 //start a temporary database
@@ -25,7 +25,13 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"           // replace print() in python
+	"fmt" // replace print() in python
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"html/template" // for rendering HTML templates
 	"log"           // for error reporting
 	"minitwit/api"
@@ -34,13 +40,6 @@ import (
 	"strings"
 	"time"
 	_ "time/tzdata"
-
-	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Configuration struct {
@@ -296,8 +295,6 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var flashes []string
-
 	if r.Method == http.MethodPost {
 		r.ParseForm()
 		username := r.FormValue("username")
@@ -310,15 +307,12 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		if dberr != nil {
 			if dberr == mongo.ErrNoDocuments {
 				log.Println("User not found:", username)
-				flashes = append(flashes, "Invalid username")
 			} else {
-				flashes = append(flashes, "Database error occurred")
 				log.Println("Database error:", dberr)
 			}
 		} else {
 			if !checkPasswordHash(password, foundUser.HashedPW) {
 				log.Println("Password doesn't match")
-				flashes = append(flashes, "Invalid password")
 			} else {
 				session, _ := store.Get(r, "minitwit-session")
 				session.Values["user_id"] = foundUser.ID.Hex()
@@ -329,12 +323,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	data := TimelineUserData{
-		PageTitle: "Sign In",
-		Flashes:   flashes,
-	}
-
-	RenderTemplate(w, "login.html", data)
+	RenderTemplate(w, "login.html", nil)
 }
 
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
