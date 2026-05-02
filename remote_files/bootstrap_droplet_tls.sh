390080049bb71fe72f15751c92b79206af5044a0
AntohaY <antonyakovenko2401@gmail.com>
2026-05-02 21:47:27 +0200

    access grafana from /grafana url instead of :3000 and switching to http

--- remote_files/bootstrap_droplet_tls.sh
+++ remote_files/bootstrap_droplet_tls.sh
@@ -46,6 +46,22 @@ server {
 
     server_name ${SERVER_NAME};
 
+    location = /grafana {
+        return 301 /grafana/;
+    }
+
+    location /grafana/ {
+        proxy_pass http://127.0.0.1:3000;
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_set_header X-Forwarded-Prefix /grafana;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+
     location / {
         proxy_pass http://127.0.0.1:${APP_UPSTREAM_PORT};
         proxy_set_header Host \$http_host;
